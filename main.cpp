2ea5ba7727592854114ebd7bd57422e83e7e6dd0
Антон Щерба <69756211+Anton-Shcherba@users.noreply.github.com>
2020-10-29 13:57:26 +0300

    optimize: depth of each node is calculated once

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
 
-int height(std::vector <int> &vec, int el, int size) {
-	if (vec[el] == -1) return size;
-	else height(vec, vec[el], ++size);
+void lenght(std::vector <int>& vec, std::vector <int>& depth, int el) {
+	if (vec[el] == -1) depth[el] = 1;
+	else if (depth[vec[el]] != -1) depth[el] = depth[vec[el]] + 1;
+	else {
+		lenght(vec, depth, vec[el]);
+		depth[el] = depth[vec[el]] + 1;
+	}
 }
 
 int main()
@@ -11,15 +15,17 @@ int main()
 	int num;
 	std::cin >> num;
 	std::vector <int> vec;
+	std::vector <int> depth;
 	while (num--) {
 		int tmp;
 		std::cin >> tmp;
 		vec.push_back(tmp);
+		depth.push_back(-1);
 	}
 	int max = 1;
 	for (int i = 0; i < vec.size(); i++) {
-		int tmp = height(vec, i, 1);
-		if (tmp > max) max = tmp;
+		if (depth[i] == -1) lenght(vec, depth, i);
+		if (depth[i] > max) max = depth[i];
 	}
 	std::cout << max;
 	return 0;
